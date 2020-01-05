293cf11c40e766284c58aa839b70cafe08112c6d
Tom McKinney <tom@upstreamcode.com>
2020-01-05 15:34:55 -0500

    WIP

--- nuget-publish/src/main.ts
+++ nuget-publish/src/main.ts
@@ -1,11 +1,13 @@
 import * as core from '@actions/core'
-import {promisify} from 'util'
-import {exec} from 'child_process'
+import * as exec from '@actions/exec'
+// import {promisify} from 'util'
+// import {exec} from 'child_process'
 
 async function run(): Promise<void> {
   try {
     const projectPath: string = core.getInput('project-path')
-    await promisify(exec)(`dotnet test ${projectPath}`)
+    // await promisify(exec)(`dotnet test ${projectPath}`)
+    await exec.exec('dotnet', ['test', projectPath])
   } catch (error) {
     core.setFailed(error.message)
   }
